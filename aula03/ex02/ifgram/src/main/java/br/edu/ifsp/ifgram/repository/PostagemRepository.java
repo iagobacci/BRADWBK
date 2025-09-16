4f49740608ef4a389efbe1eb376add0aa7ebac12
Iago Bacci <135689143+iagobacci@users.noreply.github.com>
2025-09-16 00:15:36 -0300

    Delete aula03 directory

--- aula03/ex02/ifgram/src/main/java/br/edu/ifsp/ifgram/repository/PostagemRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifsp.ifgram.repository;
-
-import br.edu.ifsp.ifgram.model.Postagem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-}
